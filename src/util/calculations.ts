b75982c1a3f6c26094e83cff0b4e7ebecc032f36
utujesandrine456 <utujesandrine456@gmail.com>
2026-06-24 12:04:18 +0200

    updated calculations

--- src/util/calculations.ts
+++ src/util/calculations.ts
@@ -16,12 +16,12 @@ export const calculatePercentage = (stats: {
 
   const percentages = [
     (stats.abaje / stats.abanditswe) * 100,
-    (stats.abizeKarindwi / stats.abanditswe) * 100,
-    (stats.abatangiyeIsabato / stats.abanditswe) * 100,
-    (stats.abasuye / stats.abanditswe) * 100,
-    (stats.abasuwe / stats.abanditswe) * 100,
-    (stats.abafashije / stats.abanditswe) * 100,
-    (stats.abafashijwe / stats.abanditswe) * 100,
+    (stats.abizeKarindwi / stats.abaje) * 100,
+    (stats.abatangiyeIsabato / stats.abaje) * 100,
+    (stats.abasuye / stats.abaje) * 100,
+    (stats.abasuwe / stats.abaje) * 100,
+    (stats.abafashije / stats.abaje) * 100,
+    (stats.abafashijwe / stats.abaje) * 100,
   ];
 
   const sumOfPercentages = percentages.reduce(
